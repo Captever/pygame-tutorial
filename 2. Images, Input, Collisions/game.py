6f50f3a317838db2886951c8d59f09e3e0cca79f
Captever-Desktop <captever6@gmail.com>
2024-12-17 13:25:28 +0900

    practice collision

--- 2. Images, Input, Collisions/game.py
+++ 2. Images, Input, Collisions/game.py
@@ -18,9 +18,20 @@ class Game:
         self.movement = [False, False] # up, down movement
         self.move_speed = 5
 
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248)) # fill with rgb color value
+            self.screen.fill((14, 219, 248)) # fill with rgb colorkey
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height()) # x, y, width, height
+            # Same as; img_r = pygame.Rect(*self.img_pos, *self.img.get_size())
+
+            # if the image touches the collision area
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
 
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * self.move_speed
             self.screen.blit(self.img, self.img_pos) # zero by top-left
