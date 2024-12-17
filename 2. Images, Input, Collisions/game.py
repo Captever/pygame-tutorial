0b4818f429950097129625447e8d3f9059f8444e
Captever-Desktop <captever6@gmail.com>
2024-12-17 12:36:57 +0900

    Put images and controll input keys

--- 2. Images, Input, Collisions/game.py
+++ 2. Images, Input, Collisions/game.py
@@ -11,12 +11,37 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.img = pygame.image.load('../resources/data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0)) # black to transparency
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False] # up, down movement
+        self.move_speed = 5
+
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248)) # fill with rgb color value
+
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * self.move_speed
+            self.screen.blit(self.img, self.img_pos) # zero by top-left
+
             for event in pygame.event.get():
+                # when quit button is pressed
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                # when the key is pressed down
+                if event.type == pygame.KEYDOWN:
+                    if event.type == pygame.K_w:
+                        self.movement[0] = True
+                    if event.type == pygame.K_s:
+                        self.movement[1] = True
+                # when the key is pressed up
+                if event.type == pygame.KEYUP:
+                    if event.type == pygame.K_w:
+                        self.movement[0] = False
+                    if event.type == pygame.K_s:
+                        self.movement[1] = False
             
             pygame.display.update()
             self.clock.tick(60)
